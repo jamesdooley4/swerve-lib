cad0124988b8800932b042efb52d21d6029f7792
jamesdooley4 <56771024+jamesdooley4@users.noreply.github.com>
2022-02-19 12:50:32 -0800

    Fix typo

--- src/main/java/com/swervedrivespecialties/swervelib/AbsoluteEncoder.java
+++ src/main/java/com/swervedrivespecialties/swervelib/AbsoluteEncoder.java
@@ -5,7 +5,7 @@ public interface AbsoluteEncoder {
     /**
      * Gets the current angle reading of the encoder in radians.
      *
-     * @return The current angle in radians. Range: [0, 2pi)
+     * @return The current angle in radians. Range: [0, 2pi]
      */
     double getAbsoluteAngle();
 }
