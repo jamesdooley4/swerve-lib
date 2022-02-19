e4944f5f503924fbb154cd4b8a3cdbb43cb2af74
jamesdooley4 <56771024+jamesdooley4@users.noreply.github.com>
2022-02-19 12:51:00 -0800

    Add comment about returning NaN if module doesn't support returning absolute encoder position

--- src/main/java/com/swervedrivespecialties/swervelib/SwerveModuleFactory.java
+++ src/main/java/com/swervedrivespecialties/swervelib/SwerveModuleFactory.java
@@ -91,6 +91,8 @@ public class SwerveModuleFactory<DriveConfiguration, SteerConfiguration> {
             steerController.setReferenceAngle(steerAngle);
         }
 
+        // @return The current angle in radians. Range: [0, 2pi]. Returns Double.NaN if the steer controller doesn't
+        //         support returning the absolute encoder position
         @Override
         public double getAbsoluteAngle() {
             if (steerController instanceof AbsoluteEncoder) {
